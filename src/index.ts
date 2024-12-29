2b357b8165022e7eba58d4e5573880463772fec8
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-12-30 01:08:07 +0530

    Initital commit.

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/send-user-data", (req, res) => {
+    console.log("API Hit")
+    const body = req.body;
+    console.log("Received body:", body);
+
+    res.json({message:"Hello"})
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
